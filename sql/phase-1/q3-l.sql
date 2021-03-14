a30703c38bd84127d255f784f81c4260550c56bd
JustinLohKingWei <60013942+JustinLohKingWei@users.noreply.github.com>
2021-03-14 16:31:51 -0400

    FINISHED 3L

--- sql/phase-1/q3-l.sql
+++ sql/phase-1/q3-l.sql
@@ -1,73 +1,87 @@
 /*Number Of Actors*/
 CREATE VIEW no_of_actors AS
-SELECT M.title AS title, COUNT(A.mid) as actorsCount
+SELECT M.mid AS mid, COUNT(A.mid) as actorsCount
 FROM movies M
 JOIN actors A
 ON M.mid = A.mid                
-GROUP BY M.title;
-
--- DROP VIEW no_of_actors;
--- DROP VIEW no_of_common_actors;
-
-/*Common Actors*/
--- SELECT M1.title, M2.title, A1.name
--- FROM movies M1 ,movies M2, actors A1,actors A2
--- WHERE m1.mid = a1.mid and m2.mid =a2.mid and a1.name=a2.name and M1.title != M2.title;
+GROUP BY M.mid;
 
 /*No Of Common Actors*/
 CREATE VIEW no_of_common_actors AS
-SELECT M1.title AS T1 , M2.title AS T2 , count(A1.name) AS commonActor
+SELECT M1.mid AS mid1 , M2.mid AS mid2 , count(A1.name) AS commonActor
 FROM movies M1 ,movies M2, actors A1,actors A2
-WHERE m1.mid = a1.mid and m2.mid =a2.mid and a1.name=a2.name and M1.title != M2.title
-GROUP BY M1.title , M2.title;
+WHERE m1.mid = a1.mid and m2.mid =a2.mid and a1.name=a2.name and M1.mid != M2.mid
+GROUP BY M1.mid , M2.mid;
 
 /* Common actor fraction*/
-SELECT M1.title , M2.title , (N2.commonActor/N1.actorsCount) AS diff
+CREATE VIEW fraction_of_common_actors AS
+SELECT M1.mid , M2.mid , (CAST(N2.commonActor AS float)/CAST(N1.actorsCount AS float)) AS diff
 FROM movies M1 , movies M2 ,no_of_actors N1, no_of_common_actors N2
-WHERE M1.title = N1.title AND M1.title = N2.T1 AND M2.title=N2.T2 AND (N2.commonActor/N1.actorsCount)!=0 AND M1.title != M2.title; ;
-
-/*Returns actor list*/
--- SELECT M.title, A.name
--- FROM movies M, actors A
--- WHERE A.mid = M.mid;
+WHERE M1.mid = N1.mid AND M1.mid = N2.mid1 AND M2.mid=N2.mid2 AND M1.mid != M2.mid;
 
 /*Number Of Tags*/
 CREATE VIEW no_of_tags AS
-SELECT M.title, COUNT(TT.tag) AS tagCount
+SELECT M.mid , COUNT(TT.tag) AS tagCount
 FROM movies M, tags T, tag_names TT
 WHERE T.tid = TT.tid AND T.mid = M.mid               
-GROUP BY M.title;
+GROUP BY M.mid;
 
 /*No Of Common Tags*/
 CREATE VIEW no_of_common_tags AS
-SELECT M1.title, M2.title, count(TT1.tag)
+SELECT M1.mid, M2.mid, count(TT1.tag) AS commonTag
 FROM movies M1 ,movies M2,  tags T1, tags T2, tag_names TT1, tag_names TT2
-WHERE M1.mid = T1.mid and M2.mid = T2.mid and TT1.tag = TT2.tag and TT1.tid = T1.tid and TT2.tid = T2.tid and M1.title != M2.title
-GROUP BY M1.title , M2.title;
+WHERE M1.mid = T1.mid and M2.mid = T2.mid and TT1.tag = TT2.tag and TT1.tid = T1.tid and TT2.tid = T2.tid and M1.mid != M2.mid
+GROUP BY M1.mid , M2.mid;
+
+
+/* Common tag fraction*/
+CREATE VIEW fraction_of_common_tags AS
+SELECT M1.mid , M2.mid , (CAST(N2. commonTag AS float)/CAST(N1.tagCount AS float)) AS diff
+FROM movies M1 , movies M2 ,no_of_tags N1, no_of_common_tags N2
+WHERE M1.mid = N1.mid AND M1.mid = N2.mid1 AND M2.mid=N2.mid2 AND M1.mid != M2.mid;
 
 /*Number of Genres*/
 CREATE VIEW no_of_genres AS
-SELECT M.title, COUNT(G.genre) as genreCount
+SELECT M.mid AS mid , COUNT(G.genre) as genreCount
 FROM movies M
 JOIN Genres G
 ON M.mid = G.mid                
-GROUP BY M.title;
+GROUP BY M.mid;
 
 /*No Of Common Genres*/
 CREATE VIEW no_of_common_genres AS
-SELECT M1.title, M2.title, count(G1.genre) 
+SELECT M1.mid AS mid1 , M2.mid AS mid2 , count(G1.genre) AS commonGenre
 FROM movies M1 ,movies M2, Genres G1, Genres G2
-WHERE m1.mid = G1.mid  and m2.mid = G2.mid and G1.genre = G2.genre and M1.title != M2.title
-GROUP BY M1.title , M2.title;
+WHERE m1.mid = G1.mid  and m2.mid = G2.mid and G1.genre = G2.genre and M1.mid!= M2.mid
+GROUP BY M1.mid , M2.mid;
+
+/* Common Genre fraction*/
+CREATE VIEW fraction_of_common_genres AS
+SELECT M1.mid , M2.mid , (CAST(N2.commonGenre AS float)/CAST(N1.genreCount AS float)) AS diff
+FROM movies M1 , movies M2 ,no_of_genres N1, no_of_common_genres N2
+WHERE M1.mid = N1.mid AND M1.mid = N2.mid1 AND M2.mid=N2.mid2 AND M1.mid != M2.mid;
 
 /*Normalized year difference*/
-CREATE VIEW year_diff AS
-SELECT M1.title, M2.title, ABS(M1.year-M2.year) AS diff
+CREATE VIEW year_diff AS 
+SELECT M1.mid AS mid1, M2.mid AS mid2 ,M1.year, M2.year,1-(ABS(CAST( (M1.year-M2.year)As float))/CAST( (M1.year)As float)) AS diff
 FROM movies M1 ,movies M2
-WHERE M1.title != M2.title
+WHERE M1.mid  != M2.mid 
 
 /*Normalized rating difference*/
 CREATE VIEW rating_diff AS
-SELECT M1.title, M2.title, ABS(M1.rating-M2.rating) AS diff
+SELECT M1.mid AS mid1, M2.mid AS mid2,DECODE(m1.rating, 0, 0,1-(ABS(M1.rating-M2.rating)/M1.rating))  AS diff
 FROM movies M1 ,movies M2
-WHERE M1.title != M2.title
+WHERE M1.mid != M2.mid 
+
+/*Similarity Percentage View*/
+CREATE VIEW similarity_percentage AS
+SELECT M1.mid AS mid1 , M2.mid AS mid2 , (f1.diff + f2.diff + f3.diff + YD.diff + RD.diff)*100.0/5.0 AS Sim
+FROM movies M1,movies M2,fraction_of_common_actors f1,fraction_of_common_tags f2, fraction_of_common_genres f3,year_diff YD ,rating_diff RD
+WHERE M1.mid = f1.mid1 AND M2.mid = f1.mid2 AND M1.mid = f2.mid1 AND M2.mid = f2.mid2 AND M1.mid = f3.mid1 AND M2.mid = f3.mid2 AND M1.mid = YD.mid1 AND M2.mid = YD.mid2 AND M1.mid = RD.mid1 AND M2.mid = RD.mid2 AND M1.mid  != M2.mid 
+
+/* Query*/
+SELECT M2.title, M2.rating,S.Sim
+FROM similarity_percentage S, movies M1,movies M2
+WHERE S.mid1 = m1.mid AND m1.title= 'Mr. & Mrs. Smith' AND S.mid2=M2.mid
+ORDER BY S.Sim DESC
+LIMIT 10
