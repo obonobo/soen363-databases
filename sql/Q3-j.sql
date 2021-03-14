6e4384cf780cf3d78667c232fa4ac6a5b610885e
TonyMontana11 <anth.chraim@gmail.com>
2021-03-14 19:41:51 -0400

    updated q3 part J

--- sql/Q3-j.sql
+++ sql/Q3-j.sql
@@ -9,7 +9,7 @@ AND name <> 'Annette Nicole'
 
 SELECT COUNT(1)
 FROM co_actors
---179
+--178
 
 2.
 create VIEW all_combinations AS
@@ -19,5 +19,24 @@ WHERE mid IN(SELECT mid FROM actors WHERE name = 'Annette Nicole')
 
 SELECT COUNT(1)
 FROM all_combinations
---537
+--534
 
+3.
+CREATE VIEW non_existent AS
+SELECT ac.mid, ac.name
+FROM all_combinations ac
+LEFT JOIN (SELECT ac.mid, ac.name
+FROM all_combinations ac, actors a
+WHERE a.mid = ac.mid AND a.name = ac.name) z ON z.name = ac.name AND z.mid = ac.mid
+WHERE z.mid is null
+
+SELECT COUNT(1)
+FROM non_existent
+--239
+
+4.
+SELECT ca.name
+FROM co_actors ca
+LEFT JOIN non_existent ne 
+ON ne.name = ca.name
+WHERE ne.name is null
