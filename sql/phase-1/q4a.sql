00b837bb57ace7988c4ceba44cb1003efa498d4e
Amine K <amine.kihal@gmail.com>
2021-03-14 23:11:36 -0400

    Update q4a.sql

--- sql/phase-1/q4a.sql
+++ sql/phase-1/q4a.sql
@@ -2,7 +2,5 @@ CREATE INDEX movies_idx1 ON movies (title);
 CREATE INDEX movies_idx2 ON movies (year);
 CREATE INDEX movies_idx3 ON movies (rating);
 CREATE INDEX movies_idx4 ON movies (num_rating);
-
 CREATE INDEX genres_idx1 ON movies (title);
-
 CREATE INDEX tag_name_idx1 ON tag_names (tag);
