3e1c8e4a56b4f57699d35f0ff5265277296052aa
TonyMontana11 <anth.chraim@gmail.com>
2021-03-14 20:57:08 -0400

    Added q4 Part A

---
+++ sql/phase-1/q4a.sql
@@ -0,0 +1,8 @@
+CREATE INDEX movies_idx1 ON movies (title);
+CREATE INDEX movies_idx2 ON movies (year);
+CREATE INDEX movies_idx3 ON movies (rating);
+CREATE INDEX movies_idx4 ON movies (num_rating);
+
+CREATE INDEX genres_idx1 ON movies (title);
+
+CREATE INDEX tag_name_idx1 ON tag_names (tag);
