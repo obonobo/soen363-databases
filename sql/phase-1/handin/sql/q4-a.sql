a5869f65813cc402181ffc1c39e91a98ac93e631
Amine K <amine.kihal@gmail.com>
2021-03-14 23:15:17 -0400

    Update q4-a.sql

--- sql/phase-1/handin/sql/q4-a.sql
+++ sql/phase-1/handin/sql/q4-a.sql
@@ -6,3 +6,10 @@ CREATE INDEX movies_idx4 ON movies (num_rating);
 CREATE INDEX genres_idx1 ON movies (title);
 
 CREATE INDEX tag_name_idx1 ON tag_names (tag);
+
+-- Index for question 3-L
+CREATE INDEX similarity_percentage_indx1 ON similarity_percentage(sim);
+
+-- Index for question 3-L
+
+
