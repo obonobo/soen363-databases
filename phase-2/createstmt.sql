0ed2068417e4a747a2bae089538b12e249205f23
TonyMontana11 <anth.chraim@gmail.com>
2021-04-15 23:51:54 -0400

    added ERD and table creation files

---
+++ phase-2/createstmt.sql
@@ -0,0 +1,62 @@
+CREATE TABLE movies_metadata(
+    id INTEGER NOT NULL,
+    adult BOOLEAN NOT NULL,
+    belongs_to_collection NVARCHAR,
+    budget INTEGER,
+    genre VARCHAR,
+    homepage VARCHAR,
+    imdb_id VARCHAR,
+    original_language VARCHAR(2),
+    original_title NVARCHAR,
+    overview VARCHAR,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE keywords(
+    id INTEGER NOT NULL,
+    keywords VARCHAR,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES movies_metadata(id)
+);
+
+CREATE TABLE credits(
+    id INTEGER NOT NULL,
+    cast VARCHAR,
+    crew VARCHAR,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id) REFERENCES movies_metadata(id)
+);
+
+CREATE TABLE ratings(
+    movieId INTEGER,
+    userId INTEGER,
+    rating REAL,
+    timestamp TIMETAMP,
+    PRIMARY KEY (movieId, userId)
+    FOREIGN KEY (movieId) REFERENCES movies_metadata(id)
+);
+
+CREATE TABLE rating_small(
+    movieId INTEGER,
+    userId INTEGER,
+    rating REAL,
+    timestamp TIMETAMP,
+    PRIMARY KEY (movieId, userId)
+    FOREIGN KEY (movieId) REFERENCES movies_metadata(id)
+);
+
+CREATE TABLE links(
+    movieId INTEGER,
+    imdbId INTEGER,
+    tmdbId INTEGER,
+    PRIMARY KEY (movieId)
+    FOREIGN KEY (movieId) REFERENCES movies_metadata(id)
+);
+
+CREATE TABLE links_small(
+    movieId INTEGER,
+    imdbId INTEGER,
+    tmdbId INTEGER,
+    PRIMARY KEY (movieId)
+    FOREIGN KEY (movieId) REFERENCES movies_metadata(id)
+);
